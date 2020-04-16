3f6193f3d71170309bd78e346cb27b49ffdbb93d
The Punslinger <59809715+Pun-slinger@users.noreply.github.com>
2020-04-16 13:34:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+/** Add any JavaScript you need to this file. */
+
+function Gate() {
+    // document.querySelectorAll("#door").style.visibility = "hidden";
+    // document.querySelectorAll("#gate").style.visibility = "visible";
+    var door = document.querySelectorAll(".door");
+    for(var i = 0; i < door.length; i++){
+        door[i].style.display = 'none';
+    }
+
+    var gate = document.querySelectorAll(".gate");
+    for(var i = 0; i < gate.length; i++){
+        gate[i].style.display = 'initial';
+    }
+}
+
+function Door() {
+    document.querySelectorAll("#gate").style.visibility = "hidden";
+    document.querySelectorAll("#door").style.visibility = "visible";
+}
+
+function All() {
+    document.querySelectorAll("#gate").style.visibility = "visible";
+    document.querySelectorAll("#door").style.visibility = "visible";
+}
+
+function myFunction() {
+    var x = document.getElementById("topnavbar");
+    if (x.className === "topnavbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnavbar";
+    }
+  }
